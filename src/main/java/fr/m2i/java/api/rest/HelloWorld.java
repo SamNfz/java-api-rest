b523bbd0624c80172a389e65f4d6c94401fcca96
Sami NEFZI <sami.nefzi@gmail.com>
2022-08-23 10:01:16 +0200

    Book resource done

---
+++ src/main/java/fr/m2i/java/api/rest/HelloWorld.java
@@ -0,0 +1,16 @@
+package fr.m2i.java.api.rest;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+
+@Path("/hello")
+public class HelloWorld {
+
+    @GET
+    @Produces("text/plain")
+    public String getHelloWorld() {
+        return "First API";
+    }
+
+}
