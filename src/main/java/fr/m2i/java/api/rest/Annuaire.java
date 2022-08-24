32db75c0d52d25979a4bf49d5706f32d1ccaedc0
Sami NEFZI <sami.nefzi@gmail.com>
2022-08-24 14:43:08 +0200

    Annuaire TP Done

--- src/main/java/fr/m2i/java/api/rest/Annuaire.java
+++ src/main/java/fr/m2i/java/api/rest/Annuaire.java
@@ -25,4 +25,42 @@ public class Annuaire {
     public List<Personne> getPersonnes() {
         return personnes;
     }
+
+    public Personne getPersonneById(Long id) {
+
+        for (Personne p : personnes) {
+            if (p.getId().equals(id)) {
+                return p;
+            }
+        }
+
+        return null;
+    }
+
+    public boolean update(Long id, Personne personne) {
+
+        Personne toUpdate = getPersonneById(id);
+
+        if (toUpdate == null) {
+            return false;
+        }
+
+        personnes.remove(toUpdate);
+        personne.setId(id);
+        personnes.add(personne);
+
+        return true;
+    }
+    
+    public boolean delete(Long id) {
+
+        Personne toDelete = getPersonneById(id);
+
+        if (toDelete == null) {
+            return false;
+        }
+
+        personnes.remove(toDelete);
+        return true;
+    }
 }
--- src/main/java/fr/m2i/java/api/rest/AnnuaireResource.java
+++ src/main/java/fr/m2i/java/api/rest/AnnuaireResource.java
@@ -6,6 +6,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.ws.rs.*;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 @Path("/personnes")
 public class AnnuaireResource {
@@ -50,4 +51,95 @@ public class AnnuaireResource {
         return annuaire.getPersonnes();
     }
 
+    // URI : /1
+    @GET
+    @Path("/{id}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getPersonneById(@PathParam("id") Long id, @Context HttpServletRequest request) {
+        System.out.println("Endpoint : getPersonneById");
+
+        // Vérifie le paramètre 'id' -> bad request si invalide
+        if (id == null || id < 1L) {
+            return Response.status(Response.Status.BAD_REQUEST)
+                    .entity("Le paramètre id est invalide").build();
+        }
+
+        Annuaire annuaire = (Annuaire) request.getSession().getAttribute("annuaire");
+        String notFoundError = String.format("La personne avec l'id: %d n'existe pas", id);
+
+        // L'annuaire n'est pas encore créer -> personne not found
+        if (annuaire == null) {
+            return Response.status(Response.Status.NOT_FOUND).entity(notFoundError).build();
+        }
+
+        Personne personne = annuaire.getPersonneById(id);
+
+        // Personne non trouvé dans la liste -> personne not found
+        if (personne == null) {
+            return Response.status(Response.Status.NOT_FOUND).entity(notFoundError).build();
+        }
+
+        return Response.status(Response.Status.OK).entity(personne).build();
+    }
+
+    // URI : /1
+    @PUT
+    @Path("/{id}")
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response update(@PathParam("id") Long id, Personne personne, @Context HttpServletRequest request) {
+        System.out.println("Endpoint : update");
+
+        // Vérifie le paramètre 'id' -> bad request si invalide
+        if (id == null || id < 1L) {
+            return Response.status(Response.Status.BAD_REQUEST)
+                    .entity("Le paramètre id est invalide").build();
+        }
+
+        Annuaire annuaire = (Annuaire) request.getSession().getAttribute("annuaire");
+        String notFoundError = String.format("La personne avec l'id: %d n'existe pas", id);
+
+        // L'annuaire n'est pas encore créer -> personne not found
+        if (annuaire == null) {
+            return Response.status(Response.Status.NOT_FOUND).entity(notFoundError).build();
+        }
+
+        boolean success = annuaire.update(id, personne);
+
+        if (!success) {
+            return Response.status(Response.Status.NOT_FOUND).entity(notFoundError).build();
+        }
+
+        return Response.status(Response.Status.NO_CONTENT).build();
+    }
+
+    // URI : /1
+    @DELETE
+    @Path("/{id}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response deletePersonne(@PathParam("id") Long id, @Context HttpServletRequest request) {
+        System.out.println("Endpoint : deletePersonne");
+
+        // Vérifie le paramètre 'id' -> bad request si invalide
+        if (id == null || id < 1L) {
+            return Response.status(Response.Status.BAD_REQUEST)
+                    .entity("Le paramètre id est invalide").build();
+        }
+
+        Annuaire annuaire = (Annuaire) request.getSession().getAttribute("annuaire");
+        String notFoundError = String.format("La personne avec l'id: %d n'existe pas", id);
+
+        // L'annuaire n'est pas encore créer -> personne not found
+        if (annuaire == null) {
+            return Response.status(Response.Status.NOT_FOUND).entity(notFoundError).build();
+        }
+
+        boolean success = annuaire.delete(id);
+
+        if (!success) {
+            return Response.status(Response.Status.NOT_FOUND).entity(notFoundError).build();
+        }
+
+        return Response.status(Response.Status.OK).entity("La personne a bien été supprimé").build();
+    }
 }
